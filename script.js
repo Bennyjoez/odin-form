97ad5b8465fc489fc512cf8b780046109b921649
Benson Njuguna <njugunab655@gmail.com>
2022-10-08 10:26:16 +0300

    Change the confirm function in script.js file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const password = document.querySelector('#password');
 const passwordConfirm = document.querySelector('#confirm-password');
 const submitButton = document.querySelector('button[type="submit"]');
+const passwordConfirmMessage = document.querySelector('p[class="password-match"]');
 
 submitButton.addEventListener('click', alertStatus) 
 function alertStatus() {
@@ -12,8 +13,24 @@ function alertStatus() {
 passwordConfirm.addEventListener('keyup', confirm);
 
 function confirm() {
-    if(password.value != '') {
+    const accountPassword = password.value;
+    const confirmPassword = passwordConfirm.value;
+
+    if(accountPassword != '') {
         const accountPassword = password.value;
         passwordConfirm.setAttribute('pattern', `${accountPassword}`)
-    }
+        if(confirmPassword.length === accountPassword.length) {
+            if(confirmPassword === accountPassword) {
+                passwordConfirmMessage.textContent = 'Passwords match';
+                passwordConfirmMessage.classList.remove("password-dontmatch-css");
+                passwordConfirmMessage.classList.add('password-match-css');
+            } else {
+                passwordConfirmMessage.textContent = "Password don't match";
+                passwordConfirmMessage.classList.remove("password-match-css");
+                passwordConfirmMessage.classList.add('password-dontmatch-css');
+            }
+        }
+    } 
+
+
 }
